0f66e37c80241f0afd4fcb2333e8049c1018fbfc
githubjenkins <githubjenkins409@gmail.com>
2022-05-13 10:26:15 +0530

    changing the code

--- Power.java
+++ Power.java
@@ -2,6 +2,8 @@ class Power{
  public static void main(String args[]) {
    System.out.println(" *  *  *");
    System.out.println(" Power ");
+     System.out.println(" Power ");
+  System.out.println(" --------");
   }
 }
 
